d86c38d286ef7bd1f46848e6583abcf9b93a3ede
Ubuntu <princepaulite@gmail.com>
2019-02-28 12:25:55 +0000

    Added dump data

---
+++ dump/envSetup.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+#cd /home/ubuntu/virtualenvironment/$1/bin/
+#source activate
+#cd
+
+source /home/ubuntu/virtualenvironment/tfenv/bin/activate
+cd /home/ubuntu/projects/psychic-spoon/code
---
+++ dump/scripts/envSetup.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+#cd /home/ubuntu/virtualenvironment/$1/bin/
+#source activate
+#cd
+
+source /home/ubuntu/virtualenvironment/tfenv/bin/activate
